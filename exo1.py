a1e5558952996f2d24af49c1cc4a4eaa46b99831
hdlf <percevallechat@gmail.com>
2018-11-05 19:14:10 +0100

    exochats fonction

--- exo1.py
+++ exo1.py
@@ -1,5 +1,17 @@
 print("hello world")
 
-chat="Perceval"
+def isChat(chatparametres):
+    if chatparametres =="Perceval":
+        print("bebe")
+    else:
+        print("pas bebe")
+
+chat="Ivitch"
+
+chat2="Perceval"
 
 print(chat)
+isChat(chat)
+
+print(chat2)
+isChat(chat2)
