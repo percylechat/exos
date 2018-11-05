d0de375e297ce01357a12362dce2bae85d76f2b5
hdlf <percevallechat@gmail.com>
2018-11-05 19:19:09 +0100

    nouveauxchats

--- exo1.py
+++ exo1.py
@@ -3,15 +3,22 @@ print("hello world")
 def isChat(chatparametres):
     if chatparametres =="Perceval":
         print("bebe")
+    elif chatparametres =="Ivitch":
+        print("princesse")
     else:
-        print("pas bebe")
+        print("chat inconnu")
 
 chat="Ivitch"
 
 chat2="Perceval"
+
+chat3="Robert"
 
 print(chat)
 isChat(chat)
 
 print(chat2)
 isChat(chat2)
+
+print(chat3)
+isChat(chat3)
